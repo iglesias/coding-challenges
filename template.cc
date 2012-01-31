4786639fa0a0b8b9e0a313c5b68aaa898b3e54a3
iglesias <fernando.iglesiasg@gmail.com>
2012-01-31 22:55:47 +0100

    + template for fast coding with C++

---
+++ template.cc
@@ -0,0 +1,39 @@
+// {{{ Boilerplate Code <--------------------------------------------------
+
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <utility>
+#include <vector>
+
+#define   FOR(i, a, b)    for ( typeof(a) i = (a) ; i < (b) ; ++i )
+#define   REP(i, n)       FOR(i, 0, n)
+#define   ALL(a)          (a).begin(), (a).end()
+
+using namespace std;
+
+typedef pair< int, int >  ii;
+typedef long long         ll;
+
+// }}}
+
+int main()
+{
+  cout << "Hello template!" << endl;
+
+  return 0;
+}
