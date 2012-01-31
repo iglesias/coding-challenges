923edc86f3c6cfb6f26c33c2e12ee4ba2d7475f2
iglesias <fernando.iglesiasg@gmail.com>
2012-01-31 23:00:19 +0100

    + very basic template for fast coding with Java

---
+++ Template.java
@@ -0,0 +1,19 @@
+import static java.lang.Math.*;
+import static java.math.BigInteger.*;
+import static java.util.Arrays.*;
+import static java.util.Collections.*;
+import java.math.*;
+import java.util.*;
+
+//TODO Add fast input and output stuff
+
+public class Template {
+
+  public static void main(String [] args) {
+  
+    System.out.println("Hello template!");
+
+  }
+
+
+}
