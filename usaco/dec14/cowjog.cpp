3c002cf6437b945fa9d044ff882c21b4e59ea102
Fernando Iglesias <fernando.iglesiasg@gmail.com>
2015-08-16 19:49:57 +0200

    USACO december 2014 contest (solved back then; too lazy to upload in the past).

---
+++ usaco/dec14/cowjog.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+typedef long long int ll;
+
+using namespace std;
+
+int main()
+{
+    ifstream fin;
+    fin.open("cowjog.in");
+    int N;
+    fin >> N;
+    vector<ll> vll(N);
+    for (int i=0; i<N; i++)
+    {
+        ll p, v;
+        fin >> p >> v;
+        vll[i] = v;
+    }
+    fin.close();
+
+    int idx = N-1;
+    int ans = N;
+    while (idx > 0)
+    {
+        ll v = vll[idx];
+        while (--idx >= 0)
+            if (vll[idx] > v)
+                ans--;
+            else
+                break;
+    }
+
+    ofstream fout;
+    fout.open("cowjog.out");
+    fout << ans << endl;
+    fout.close();
+
+    return 0;
+}
