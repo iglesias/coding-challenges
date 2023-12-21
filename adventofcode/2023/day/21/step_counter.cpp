84cef3a4ab03a88ae60242666abf99e17293d43a
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2023-12-21 09:49:10 +0100

    Playing step by step.
    - Use formula for mod of negative numbers iso loop.
    - set -> unordered_set with a bit of boost for minimal handcode.
    Neither had a sensible impact running with sample input for 500 steps.
    It takes several seconds up to a say a minute.
    With 1000 steps is not finishing while I commit this.
    We may expand the ones that are surely going to continue going far with
    math and then do the simulation for the others? Hmm.

--- adventofcode/2023/day/21/step_counter.cpp
+++ adventofcode/2023/day/21/step_counter.cpp
@@ -1,17 +1,18 @@
 // {{{ Boilerplate Code <--------------------------------------------------------------------------
 
+
 #include <bits/stdc++.h>
+
+#include <boost/functional/hash.hpp>
 
 #define   FOR(i, a, b)    for ( decltype(a) i = (a) ; i < (b) ; ++i )
 #define   REP(i, n)       FOR(i, 0, n)
 
 using ii = std::pair<int, int>;
 
-// ------------------------------------------------------------------------------> End of Boilerplate }}}
-
 void read_input();
 std::pair<int, unsigned long long> ans;
-void solve();
+void solve(int num_steps);
 
 using namespace std;
 
@@ -27,10 +28,11 @@ void print(grid const& g)
   cout << "\n\n";
 }
 
-int main()
+int main(int argc, char* argv[])
 {
   read_input();
-  solve();
+  int const num_steps = argc > 1 ? std::stoi(argv[1]) : 64;
+  solve(num_steps);
   std::cout << "Part one: " << ans.first << "\nPart two: " << ans.second << '\n';
 }
 
@@ -45,7 +47,7 @@ void read_input()
   }
 }
 
-void solve()
+void solve(int num_steps)
 {
   ii start;
   REP(r, R) REP(c, C) if(G[r][c] == 'S') start = make_pair(r, c);
@@ -76,7 +78,7 @@ void solve()
 
   auto make_new_positions = [&deltas](grid const& G, queue<ii>& q) -> queue<ii> {
     queue<ii> nq;
-    set<ii> seen;
+    unordered_set<ii, boost::hash<ii>> seen;
     while(!q.empty()){
       auto p = q.front();
       q.pop();
@@ -84,9 +86,13 @@ void solve()
       seen.insert(p);
       for(auto const& delta : deltas){
         int r = p.first + delta.first, c = p.second + delta.second;
-        if(not(0 <= r and r < R and 0 <= c and c < C)) continue;
         if(seen.contains({r, c})) continue;
-        if(G[r][c] != '#'){
+        // In part two the map is infinite:
+        //if(not(0 <= r and r < R and 0 <= c and c < C)) continue;
+        int rr = r, cc = c;
+        rr = (rr%R+R)%R; //while(rr < 0) rr += R;
+        cc = (cc%C+C)%C; //while(cc < 0) cc += C;
+        if(G[rr][cc] != '#'){
           nq.emplace(r, c);
           seen.emplace(r, c);
         }
@@ -107,17 +113,18 @@ void solve()
     print(g);
     int positions = 0;
     REP(r, R) REP(c, C) if(g[r][c] == 'O') positions++;
-    std::cout << positions << " " << q.size() << "\n";
+    ans.first = static_cast<int>(q.size());
   }
 
   {
     queue<ii> q;
     q.push(start);
     grid g;
-    for(int i = 0; i < 64; i++){
+    for(int i = 0; i < num_steps; i++){
       auto nq = make_new_positions(G,q);
       q = nq;
     }
-    ans.first = static_cast<int>(q.size());
+
+    ans.second = static_cast<int>(q.size());
   }
 }
