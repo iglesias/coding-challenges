408de769ecfc5a8f13d654488fd96d6bc4919d65
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2023-12-21 08:44:18 +0100

    Toward part two making the moved-to positions not explicit in a grid.
    It is tricky, done experiment-driven, to filter seen in q to get the
    right number.

--- adventofcode/2023/day/21/step_counter.cpp
+++ adventofcode/2023/day/21/step_counter.cpp
@@ -1,7 +1,4 @@
 // {{{ Boilerplate Code <--------------------------------------------------------------------------
-//
-//
-// Basic ------------------------------------------------------------------------------------------
 
 #include <bits/stdc++.h>
 
@@ -10,8 +7,10 @@
 
 using ii = std::pair<int, int>;
 
+// ------------------------------------------------------------------------------> End of Boilerplate }}}
+
 void read_input();
-ii ans;
+std::pair<int, unsigned long long> ans;
 void solve();
 
 using namespace std;
@@ -53,10 +52,9 @@ void solve()
 
   vector<ii> const deltas{{0,1}, {0,-1}, {1,0} , {-1,0}};
 
-  auto f = [&deltas](grid const& G, grid& G1, queue<ii>& q) -> pair<queue<ii>, int> {
+  auto paint_grid = [&deltas](grid const& G, grid& g, queue<ii>& q) -> queue<ii> {
     queue<ii> nq;
     set<ii> seen;
-    int num_pos = 0;
     while(!q.empty()){
       auto p = q.front();
       q.pop();
@@ -64,34 +62,62 @@ void solve()
       seen.insert(p);
       for(auto const& delta : deltas){
         int r = p.first + delta.first, c = p.second + delta.second;
-        if(0 <= r and r < R and 0 <= c and c < C and G[r][c] != '#'){
-          G1[r][c] = 'O';
-          num_pos++;
+        if(not(0 <= r and r < R and 0 <= c and c < C)) continue;
+        if(seen.contains({r, c})) continue;
+        if(G[r][c] != '#'){
+          g[r][c] = 'O';
           nq.emplace(r, c);
+          seen.emplace(r, c);
         }
       }
     }
-    return make_pair(nq, num_pos);
+    return nq;
   };
 
-  /*
-  G1 = G;
-  auto [q2, n2] = f(G,G1,q1);
-  print(G1);
+  auto make_new_positions = [&deltas](grid const& G, queue<ii>& q) -> queue<ii> {
+    queue<ii> nq;
+    set<ii> seen;
+    while(!q.empty()){
+      auto p = q.front();
+      q.pop();
+      if(seen.contains(p)) continue;
+      seen.insert(p);
+      for(auto const& delta : deltas){
+        int r = p.first + delta.first, c = p.second + delta.second;
+        if(not(0 <= r and r < R and 0 <= c and c < C)) continue;
+        if(seen.contains({r, c})) continue;
+        if(G[r][c] != '#'){
+          nq.emplace(r, c);
+          seen.emplace(r, c);
+        }
+      }
+    }
+    return nq;
+  };
 
-  G1 = G;
-  auto [q3, n3] = f(G,G1,q2);
-  print(G1);
-  */
-
-  queue<ii> q;
-  q.push(start);
-  grid g;
-  for(int i = 0; i < 64; i++){
-    g = G;
-    auto [nq, nn] = f(G,g,q);
-    q = nq;
+  {
+    queue<ii> q;
+    q.push(start);
+    grid g;
+    for(int i = 0; i < 64; i++){
+      g = G;
+      auto nq = paint_grid(G,g,q);
+      q = nq;
+    }
+    print(g);
+    int positions = 0;
+    REP(r, R) REP(c, C) if(g[r][c] == 'O') positions++;
+    std::cout << positions << " " << q.size() << "\n";
   }
-  print(g);
-  REP(r, R) REP(c, C) if(g[r][c] == 'O') ans.first++;
+
+  {
+    queue<ii> q;
+    q.push(start);
+    grid g;
+    for(int i = 0; i < 64; i++){
+      auto nq = make_new_positions(G,q);
+      q = nq;
+    }
+    ans.first = static_cast<int>(q.size());
+  }
 }
