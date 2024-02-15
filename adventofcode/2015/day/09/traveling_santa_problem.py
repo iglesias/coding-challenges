748c326384f3215ccafc2a51079db8542072b850
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-02-15 13:04:49 +0100

    Refactor.

--- adventofcode/2015/day/09/traveling_santa_problem.py
+++ adventofcode/2015/day/09/traveling_santa_problem.py
@@ -6,48 +6,57 @@ import numpy as np
 import unittest
 
 from collections import deque
+from math import factorial
+from time import time
 
 ''' Over-engineered input reading '''
 
-# FIXME class input reader wrapping readinput call
-# with toidx and fromidx members
-def updatemaps(loc: str, toidx: dict[str, int], fromidx: [str]) -> None:
-    if not loc in toidx:
-        toidx[loc] = len(toidx)
-        fromidx.append(loc)
-
-def updatelist(loc0: str, loc1: str, cost: int,
-               toidx: dict[str, int],adjlist) -> None:
-    while toidx[loc0] >= len(adjlist) or toidx[loc1] >= len(adjlist):
-        adjlist.append([])
-    adjlist[toidx[loc0]].append((toidx[loc1], cost))
-    adjlist[toidx[loc1]].append((toidx[loc0], cost))
-
-def transform(line: str) -> tuple[str, str, np.uint16]:
-    org, rest = line.split(' to ')
-    dst, cost = rest.split(' = ')
-    cost = np.uint16(cost)
-    #print(f'{org} -> {dst}: {cost}')
-    return org, dst, cost
-
-def readinput() -> tuple[dict[str, int], [str]]:
+class InputReader:
     toidx: dict[str, int] = {}
     fromidx: [str] = [] # FIXME needed? With toidx.keys() I am
                         # obtaining the same as it seems that the
                         # dict preserves order of insertion, which
                         # is how fromidx is filled.
     adjlist: [[(int, np.uint16)]] = []
-    for line in open(0).readlines():
-        org, dst, cost = transform(line)
-        updatemaps(org, toidx, fromidx)
-        updatemaps(dst, toidx, fromidx)
-        updatelist(org, dst, cost, toidx, adjlist)
-    return toidx, fromidx, adjlist
 
-class TestInput(unittest.TestCase):
+    @staticmethod
+    def updmaps(loc: str) -> None:
+        if not loc in InputReader.toidx:
+            InputReader.toidx[loc] = len(InputReader.toidx)
+            InputReader.fromidx.append(loc)
+
+    @staticmethod
+    def updlist(loc0: str, loc1: str, cost: int) -> None:
+        while InputReader.toidx[loc0] >= len(InputReader.adjlist) or \
+                InputReader.toidx[loc1] >= len(InputReader.adjlist):
+            InputReader.adjlist.append([])
+        InputReader.adjlist[InputReader.toidx[loc0]].append((InputReader.toidx[loc1], cost))
+        InputReader.adjlist[InputReader.toidx[loc1]].append((InputReader.toidx[loc0], cost))
+
+    @staticmethod
+    def transform(line: str) -> tuple[str, str, np.uint16]:
+        org, rest = line.split(' to ')
+        dst, cost = rest.split(' = ')
+        cost = np.uint16(cost)
+        #print(f'{org} -> {dst}: {cost}')
+        return org, dst, cost
+
+    @staticmethod
+    def makedts() -> tuple[dict[str, int], [str], [[(int, np.uint16)]]]:
+        ''' Make Data structures from lines in stdin '''
+        for line in open(0).readlines():
+            org, dst, cost = InputReader.transform(line)
+            InputReader.updmaps(org)
+            InputReader.updmaps(dst)
+            InputReader.updlist(org, dst, cost)
+        return InputReader.toidx, InputReader.fromidx, InputReader.adjlist
+
+
+class TestInputReader(unittest.TestCase):
     def runTest(self):
-        self.assertEqual(transform('A to B = 3'), ('A', 'B', 3))
-        self.assertEqual(transform('edhv to ams = 200'), ('edhv', 'ams', 200))
+        ir = InputReader()
+        self.assertEqual(ir.transform('A to B = 3'), ('A', 'B', 3))
+        self.assertEqual(ir.transform('edhv to ams = 200'), ('edhv', 'ams', 200))
 
 
 def adjmat(adjlist) -> np.array:
@@ -58,37 +67,44 @@ def adjmat(adjlist) -> np.array:
             A[i,j] = cost
     return A
 
-def main():
-    toidx, fromidx, adjlist = readinput()
-    print(toidx)
-    print(fromidx)
-    print(toidx.keys())
-    print(adjlist)
-    # Number of nodes.
-    assert(len(toidx) == len(fromidx) and len(toidx) == len(adjlist))
-    N = len(toidx)
-    # Adjacency matrix.
-    A = adjmat(adjlist)
-    print(A)
+
+def bfs(A: np.array, fromidx: [str]) -> (np.uint16, np.uint16):
+    assert(A.shape[0] == A.shape[1])
+    N = A.shape[0]
     q = deque()
     for i in range(N):
         q.append((i, {i}, set(range(N))-{i},
                   np.uint16(0), [fromidx[i]]))
     mincost = np.uint16(60000)
     maxcost = np.uint16(0)
+    numpaths = 0
     while len(q) > 0:
         org, visited, pending, cost, path = q.pop()
-        print(org, visited, pending, cost)
-        print(path)
         if len(pending) == 0:
-            print(path, cost)
+            #print(path, cost)
             mincost = min(mincost, cost)
             maxcost = max(maxcost, cost)
+            numpaths += 1
         for dst in pending:
             if A[org][dst] > 0:
                 q.append((dst, visited|{dst}, pending-{dst},
                           cost+A[org][dst], path+[fromidx[dst]]))
+    print(N, factorial(N), numpaths)
     return mincost, maxcost
+
+
+def main():
+    toidx, fromidx, adjlist = InputReader.makedts()
+    assert(len(toidx) == len(fromidx) and len(toidx) == len(adjlist))
+    N = len(toidx)
+    A = adjmat(adjlist)
+    print(A)
+    startbfs = time()
+    mincost, maxcost = bfs(A, fromidx)
+    endbfs = time()
+    print(endbfs-startbfs)
+    return mincost, maxcost
+
 
 if __name__ == '__main__':
     shortest, longest = main()
