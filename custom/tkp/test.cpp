a2ab224d89062296c524f991f274547e0c036729
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-02-19 11:44:31 +0100

    Fix indentation.

--- custom/tkp/test.cpp
+++ custom/tkp/test.cpp
@@ -29,6 +29,6 @@ TEST(solve, OneAndOne)
 
 int main(int argc, char** argv)
 {
-	testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
