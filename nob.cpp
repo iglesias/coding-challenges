f730b287074a528ead7ba98b90de4e0c77d50dd6
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-04-15 20:50:01 +0200

    Add adventofcode to the build with simple traversal of subdirs.

--- nob.cpp
+++ nob.cpp
@@ -2,9 +2,11 @@
 #include "./nob.h"
 
 #include <filesystem>
+#include <queue>
+#include <string_view>
 
 #define CFLAGS    "-Wall", "-Wextra", "-std=c2x", "-pedantic"
-#define CPPFLAGS  "-Wall", "-Wextra", "-std=c++20", "-pedantic", "-Wconversion", "-Wfatal-errors", "-fsanitize=undefined,address"
+#define CPPFLAGS  "-Wall", "-Wextra", "-std=c++23", "-pedantic", "-Wconversion", "-Wfatal-errors", "-fsanitize=undefined,address"
 
 namespace fs = std::filesystem;
 
@@ -20,6 +22,27 @@ void build_kattis_c_files() {
       if (filename.ends_with(".c"))
         build_kattis_c_file(filename);
     }
+}
+
+void build_cpp_file(std::string_view filename) {
+  Cstr tool_path = PATH(filename.data());
+  CMD("g++", CPPFLAGS, "-o", NOEXT(tool_path), tool_path);
+}
+
+void build_directory_cpp_files(std::string const& root_directory) {
+  std::queue<std::string> directoriesQ;
+  directoriesQ.push(root_directory);
+  while (!directoriesQ.empty()) {
+    for (auto const& entry : fs::directory_iterator(directoriesQ.front()))
+      if (fs::is_regular_file(entry.path())) {
+        auto const filename = entry.path().filename().string();
+        if (filename.ends_with(".cc") or filename.ends_with(".cpp"))
+          build_cpp_file(directoriesQ.front() + "/" + filename);
+      } else if (fs::is_directory(entry.path())) {
+        directoriesQ.push(directoriesQ.front() + "/" + entry.path().filename().string());
+      }
+    directoriesQ.pop();
+  }
 }
 
 void run_example(const char *example) {
@@ -41,5 +64,6 @@ int main(int argc, char* argv[]) {
   GO_REBUILD_URSELF(argc, argv);
 
   build_kattis_c_files();
+  build_directory_cpp_files("adventofcode");
   run_examples();
 }
