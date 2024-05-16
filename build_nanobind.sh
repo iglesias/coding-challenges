a13530741c27e0b70e5e76c4f10686ba02f717ef
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-05-16 16:04:25 +0200

    Update build_nanobind.sh with pkg-config and cflags.

--- build_nanobind.sh
+++ build_nanobind.sh
@@ -2,8 +2,10 @@
 
 mkdir nobuild
 
-g++ nobnb.cpp -std=c++17 -fvisibility=hidden -DNDEBUG -DNB_COMPACT_ASSERTIONS -I/usr/include/python3.12 -fPIC -Iext/nanobind/include -Iext/nanobind/ext/robin_map/include -Os -c -o nobuild/nobnb.o
+PYTHON_INCLUDE=`pkg-config --cflags python3`
 
-g++ ext/nanobind/src/nb_combined.cpp -std=c++17 -fvisibility=hidden -DNDEBUG -DNB_COMPACT_ASSERTIONS -I/usr/include/python3.12 -fPIC -Iext/nanobind/include -Iext/nanobind/ext/robin_map/include -O3 -fno-strict-aliasing -ffunction-sections -fdata-sections -c -o nobuild/libnanobind.o
+g++ nobnb.cpp -std=c++17 -fvisibility=hidden -DNDEBUG -DNB_COMPACT_ASSERTIONS $PYTHON_INCLUDE -fPIC -Iext/nanobind/include -Iext/nanobind/ext/robin_map/include -Os -c -o nobuild/nobnb.o
+
+g++ ext/nanobind/src/nb_combined.cpp -std=c++17 -fvisibility=hidden -DNDEBUG -DNB_COMPACT_ASSERTIONS $PYTHON_INCLUDE -fPIC -Iext/nanobind/include -Iext/nanobind/ext/robin_map/include -O3 -fno-strict-aliasing -ffunction-sections -fdata-sections -c -o nobuild/libnanobind.o
 
 g++ -shared -Wl,-s -Wl,--gc-sections nobuild/nobnb.o nobuild/libnanobind.o -o nobuild/nob.cpython-312-x86_64-linux-gnu.so
