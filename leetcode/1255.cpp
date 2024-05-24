0e0911211e0579fed888376baf9ecc83f0d76134
Fernando J. Iglesias García <fernando.iglesiasg@gmail.com>
2024-05-24 21:52:50 +0200

    Update 1255.cpp

--- leetcode/1255.cpp
+++ leetcode/1255.cpp
@@ -10,12 +10,12 @@ std::array<int, 'z' - 'a' + 1> letters_left;  // global
 
 namespace loop {
 
-int solve(const vector<string>& words, vector<int>& scores, vector<string>& subset, size_t words_i = 0, int ans = 0)
+int solve(const vector<string>& words, const vector<int>& scores, vector<string>& subset, const size_t words_i = 0, int ans = 0)
 {
     {
         int score = 0;
-        for (const string word : subset) for (const char letter : word) score += scores[letter - 'a'];
-        ans = max(ans, score);
+        for (const string& word : subset) for (const char letter : word) score += scores[letter - 'a'];
+        ans = std::max(ans, score);
     }
 
     for (size_t j = words_i; j < words.size(); j++) {
@@ -28,7 +28,7 @@ int solve(const vector<string>& words, vector<int>& scores, vector<string>& subs
             }
         if (jump) continue;
         subset.push_back(words[j]);
-        ans = max(ans, solve(words, scores, subset, j + 1));
+        ans = std::max(ans, solve(words, scores, subset, j + 1));
         subset.pop_back();
         for (size_t i = 0; i < words[j].size(); i++) letters_left[words[j][i] - 'a']++;
     }
@@ -40,7 +40,7 @@ int solve(const vector<string>& words, vector<int>& scores, vector<string>& subs
 
 namespace without_or_with {
 
-int solve(const vector<string>& words, const vector<int>& scores, vector<string>& subset, size_t words_i = 0, int ans = 0)
+int solve(const vector<string>& words, const vector<int>& scores, vector<string>& subset, const size_t words_i = 0, int ans = 0)
 {
     {
         int score = 0;
@@ -50,7 +50,7 @@ int solve(const vector<string>& words, const vector<int>& scores, vector<string>
 
     if (words_i == words.size()) return ans;
 
-    ans = max(ans, solve(words, scores, subset, words_i + 1, ans));
+    ans = std::max(ans, solve(words, scores, subset, words_i + 1, ans));
 
     for (size_t i = 0; i < words[words_i].size(); i++)
         if (letters_left[words[words_i][i] - 'a']) letters_left[words[words_i][i] - 'a']--;
@@ -59,7 +59,7 @@ int solve(const vector<string>& words, const vector<int>& scores, vector<string>
             return ans;
         }
     subset.push_back(words[words_i]);
-    ans = max(ans, solve(words, scores, subset, words_i + 1, ans));
+    ans = std::max(ans, solve(words, scores, subset, words_i + 1, ans));
     subset.pop_back();
     for (size_t i = 0; i < words[words_i].size(); i++) letters_left[words[words_i][i] - 'a']++;
 
@@ -70,7 +70,7 @@ int solve(const vector<string>& words, const vector<int>& scores, vector<string>
 
 int max_score(const vector<string>& words, const vector<char>& letters, const vector<int>& scores, bool loop = true)
 {
-    ranges::fill(lettersLeft, 0);
+    std::ranges::fill(letters_left, 0);
     for (const char letter : letters) letters_left[letter - 'a']++;
     vector<string> subset;
     if (loop) return loop::solve(words, scores, subset);
