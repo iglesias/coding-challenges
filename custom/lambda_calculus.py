a9f2a6ac535d8baba59cf73d9000ec094aabbca7
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-05-28 20:57:15 +0200

    :lambda: Add K example, KI, and a few equivalences.

--- custom/lambda_calculus.py
+++ custom/lambda_calculus.py
@@ -12,4 +12,15 @@ def M(f):
 def K(a, b=None):
     return (lambda _: a) if b is None else a
 
+Konstant = K('onstant')
+# print(Konstant('what?'))
+
 assert K(I)('x')('y') == 'y'
+
+def KI(a, b=None):
+    return (lambda _: b) if a is None else b
+
+assert(K(M,KI) == K(M,K(I)) == M)
+assert(KI(M,K) == K(I)(M)(K) == K)
+assert(KI(K,M) == K(I)(K)(M) == M)
+
