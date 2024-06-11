4562ed6eaeb981a34705868ab95e2b30eb717a9e
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2024-06-11 21:06:48 +0200

    Add minimal splay BST (self-balancing) and parameterize benchmark.

--- adventofcode/2023/day/14/parabolic_reflector_dish.cpp
+++ adventofcode/2023/day/14/parabolic_reflector_dish.cpp
@@ -1,44 +1,45 @@
 #include <exception>
 #include <fstream>
 #include <functional>
-#include <iostream>
 #include <istream>
-//#include <map>
-#include <unordered_map>
+#include <iostream>
+#include <map>
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
 #include <benchmark/benchmark.h>
 
-void read_input(std::istream& input);
-void solve();
+#include "splay_tree_map.hpp"
+
+std::tuple<std::vector<std::string>, int, int> read_input(std::istream& input);
+template<template<typename ...> class map_t> void solve(const std::vector<std::string>&, int, int);
 
 namespace {
 
-static void main(benchmark::State& state)
+template<template<typename ...> class map_t> static void main(benchmark::State& state)
 {
   // Setup
+  std::ifstream input("./parabolic_reflector_dish_input.txt");
+  const auto& [m, R, C] = read_input(input);
   for (auto _ : state) {
-    std::ifstream input("./parabolic_reflector_dish_input.txt");
-    read_input(input);
     // SUB
-    solve();
+    solve<map_t>(m, R, C);
   }
 }
 
 }  // anonymous
 
-BENCHMARK(::main);
+BENCHMARK(::main<std::map>);
+BENCHMARK(::main<std::unordered_map>);
+BENCHMARK(::main<dts::splay_tree::map>);
 BENCHMARK_MAIN();
 
-std::vector<std::string> m;
-int R, C;
-
-void read_input(std::istream& input)
+std::tuple<std::vector<std::string>, int, int> read_input(std::istream& input)
 {
-  R = C = 0;
-  m.clear();
+  std::vector<std::string> m;
+  int R = 0, C = 0;
   std::string line;
   while(std::getline(input, line))
   {
@@ -46,28 +47,31 @@ void read_input(std::istream& input)
     R++;
     C = (int)line.length();
   }
+
+  return {m, R, C};
 }
 
-long long cycle_number;
+void cycle(std::vector<std::string>& m, int R, int C);
 
-void cycle();
-
-int try_wrap(std::unordered_map<size_t, long long>& cache)
+template<template<typename ...> class map_t>
+int try_wrap(map_t<size_t, long long>& cache,
+             std::vector<std::string>& m, int R, int C,
+             const long long cycle_number)
 {
   size_t h = std::hash<std::string>{}(m[0]);
   for(int r=1; r<R; r++) h ^= std::hash<std::string>{}(m[r]);
   if(!cache.contains(h))
   {
-    cache.insert(std::make_pair(h, cycle_number));
+    cache.emplace(h, cycle_number);
     return -1;
   }
-  for(int i = 0; i < (1000000000-cycle_number)%cache.at(h); i++) cycle();
+  for(int i = 0; i < (1000000000-cycle_number)%cache.at(h); i++) cycle(m, R, C);
   int ans = 0;
   for(int r=0;r<R;r++) for(int c=0;c<C;c++) if(m[r][c]=='O') ans += (R-r);
   return ans;
 }
 
-void tilt_north()
+void tilt_north(std::vector<std::string>& m, int R, int C)
 {
   for(int r=0;r<R;r++) for(int c=0;c<C;c++)
     if(m[r][c]=='O' and r>0 and m[r-1][c]=='.')
@@ -79,7 +83,7 @@ void tilt_north()
     }
 }
 
-void tilt_west()
+void tilt_west(std::vector<std::string>& m, int R, int C)
 {
   for(int r=0;r<R;r++) for(int c=0;c<C;c++)
     if(m[r][c]=='O' and c>0 and m[r][c-1]=='.')
@@ -91,7 +95,7 @@ void tilt_west()
     }
 }
 
-void tilt_south()
+void tilt_south(std::vector<std::string>& m, int R, int C)
 {
   for(int r=R-1;r>=0;r--) for(int c=0;c<C;c++)
     if(m[r][c]=='O' and r<R-1 and m[r+1][c]=='.')
@@ -103,7 +107,7 @@ void tilt_south()
     }
 }
 
-void tilt_east()
+void tilt_east(std::vector<std::string>& m, int R, int C)
 {
   for(int r=0;r<R;r++) for(int c=C-1;c>=0;c--)
     if(m[r][c]=='O' and c<C-1 and m[r][c+1]=='.')
@@ -115,30 +119,32 @@ void tilt_east()
     }
 }
 
-void cycle()
+void cycle(std::vector<std::string>& m, int R, int C)
 {
-  tilt_north();
-  tilt_west();
-  tilt_south();
-  tilt_east();
+  tilt_north(m, R, C);
+  tilt_west(m, R, C);
+  tilt_south(m, R, C);
+  tilt_east(m, R, C);
 }
 
-void solve()
+template<template<typename ...> class map_t>
+void solve(const std::vector<std::string>& input_m, int R, int C)
 {
   std::pair<int, int> ans{0, 0};
-  tilt_north();
+  std::vector<std::string> m = input_m; // copy
+  tilt_north(m, R, C);
   for(int r=0;r<R;r++) for(int c=0;c<C;c++) if(m[r][c]=='O') ans.first += (R-r);
-  tilt_west();
-  tilt_south();
-  tilt_east();
-  cycle_number = 1;
+  tilt_west(m, R, C);
+  tilt_south(m, R, C);
+  tilt_east(m, R, C);
+  long long cycle_number = 1;
 
-  std::unordered_map<size_t, long long> cache;
+  map_t<size_t, long long> cache;
   for(long long i=0; i<1000000000; i++)
   {
-    cycle();
+    cycle(m, R, C);
     cycle_number++;
-    if ((ans.second = try_wrap(cache)) > 0) break;
+    if ((ans.second = try_wrap(cache, m, R, C, cycle_number)) > 0) break;
   }
   //std::cout << "Part one: " << ans.first << "\nPart two: " << ans.second << '\n';
   if (ans.first != 106997 or ans.second != 99641) throw std::exception();
