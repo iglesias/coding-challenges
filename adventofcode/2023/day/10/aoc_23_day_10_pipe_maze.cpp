2ca9302fb29032a371e9aeff1aac78d93c57ae17
Fernando <fernando.iglesiasg@gmail.com>
2024-12-21 21:41:12 +0900

    Refactor and comment sanitizers.

--- adventofcode/2023/day/10/aoc_23_day_10_pipe_maze.cpp
+++ adventofcode/2023/day/10/aoc_23_day_10_pipe_maze.cpp
@@ -1,6 +1,12 @@
-#include <bits/stdc++.h>
-
-using ii = std::pair<int, int>;
+// g++ -Wall -Wextra -pedantic -Wconversion -Wfatal-errors -fsanitize=address,shadow-call-stack,undefined -fsanitize-address-use-after-scope
+#include <cassert>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
 
 std::vector<std::string> world;
 int R, C; // number of rows and columns
@@ -8,13 +14,12 @@ int R, C; // number of rows and columns
 int r_s{-1}, c_s{-1};
 void read_input();
 std::vector<std::vector<int>> numbered;
-ii ans;
-void solve();
+std::pair<int, int> solve();
 
 int main()
 {
   read_input();
-  solve();
+  std::pair<int, int> const ans = solve();
   std::cout << "Part one: " << ans.first << "\nPart two: " << ans.second << '\n';
 }
 
@@ -57,8 +62,7 @@ std::string generate_path_rec(int r, int c, std::string path)
   if(c>0 and connects_west(world[r][c]) and (!path.length() or path.back() != 'E'))
     return generate_path_rec(r, c-1, path+'W');
 
-  assert(false);
-  return path;
+  std::unreachable();
 }
 
 std::string generate_path()
@@ -67,7 +71,7 @@ std::string generate_path()
   return path;
 }
 
-void solve()
+std::pair<int, int> solve()
 {
   // Find S.
   for(int r=0; r<R; r++) for(int c=0; c<C; c++)
@@ -76,17 +80,15 @@ void solve()
 
   // Find the loop starting at S
   //   part one doesn't seem to need it explicitly, just the count
-  numbered = std::vector<std::vector<int>>(R);
-  for(int r=0; r<R; r++) numbered[r] = std::vector<int>(C, -1);
+  numbered = std::vector<std::vector<int>>(R, std::vector<int>(C, -1));
   numbered[r_s][c_s] = 0;
-  std::queue<ii> q;
+  std::queue<std::pair<int, int>> q;
   q.emplace(r_s, c_s);
 
   while(!q.empty())
   {
-    ii p = q.front();
+    auto const [r,c] = q.front();
     q.pop();
-    auto [r,c] = p;
     // south
     if(r<R-1 and connects_south(world[r][c]) and connects_north(world[r+1][c]) and numbered[r+1][c]==-1)
     {
@@ -113,6 +115,7 @@ void solve()
     }
   }
 
+  std::pair<int, int> ans{0,0};
   for(int r=0; r<R; r++)
   {
     for(int c=0; c<C; c++)
@@ -128,15 +131,18 @@ void solve()
     }
     std::cout << '\n';
   }
+  std::cout << '\n';
 
   //generate a path along the loop (I think there're at least two paths)
   auto path = generate_path();
 
   auto world_experiment = world; // it will contain the loop cells denoting its direction l(left), r(right), u(up), d(down)
-  auto isnt_loop = [&world_experiment](int r, int c) -> bool {
+  auto isnt_loop = [&world_experiment](int r, int c) -> bool
+  {
                       char const cell = world_experiment[r][c];
                       return !(cell=='d' or cell=='u' or cell=='r' or cell=='l');
-                    };
+  };
+
   {
     int r = r_s, c = c_s;
     for(char cell : path)
@@ -168,6 +174,7 @@ void solve()
         std::cout << world_experiment[r][c];
       std::cout << '\n';
     }
+    std::cout << '\n';
 
     r = r_s, c = c_s;
     for(char cell : path)
@@ -203,8 +210,9 @@ void solve()
   {
     //fill
     //color(o) another (x)
-    std::function<void(int, int, char, std::set<ii>&)> fill;
-    fill = [&world_experiment, isnt_loop, &fill](int r, int c, char color, std::set<ii>& visited) -> void {
+    using pairs_set = std::set<std::pair<int, int>>;
+    std::function<void(int, int, char, pairs_set&)> fill;
+    fill = [&world_experiment, isnt_loop, &fill](int r, int c, char color, pairs_set& visited) -> void {
       if(visited.contains(std::make_pair(r,c))) return;
       world_experiment[r][c] = color;
       visited.emplace(r, c);
@@ -213,14 +221,13 @@ void solve()
       if(c>0 and isnt_loop(r,c-1))   fill(r, c-1, color, visited);
       if(c<C-1 and isnt_loop(r,c+1)) fill(r, c+1, color, visited);
     };
-    std::set<ii> visited;
+    pairs_set visited;
     for(int r=0; r<R; r++) for(int c=0; c<C; c++)
       if(world_experiment[r][c]=='x') fill(r, c, 'x', visited);
     visited.clear();
     for(int r=0; r<R; r++) for(int c=0; c<C; c++)
       if(world_experiment[r][c]=='o') fill(r, c, 'o', visited);
 
-    std::cout << '\n';
     for(int r=0; r<R; r++)
     {
       for(int c=0; c<C; c++) std::cout << world_experiment[r][c];
@@ -228,9 +235,11 @@ void solve()
     }
     std::cout << '\n';
 
-    ii count{0,0};
+    std::pair<int, int> count{0,0};
     for(int r=0; r<R; r++) for(int c=0; c<C; c++)
       if(world_experiment[r][c]=='x')      count.first++;
       else if(world_experiment[r][c]=='o') count.second++;
+    std::cout << "x: " << count.first << " o: " << count.second << "\n";
   }
+  return ans;
 }
