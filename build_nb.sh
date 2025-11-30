f7945a5c54be93a3ded330fb1f1c23b690674c9e
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2025-11-30 21:59:40 +0100

    Update python version

--- build_nb.sh
+++ build_nb.sh
@@ -8,4 +8,4 @@ g++ nobnb.cpp -std=c++20 -fvisibility=hidden -DNDEBUG -DNB_COMPACT_ASSERTIONS $P
 
 g++ ext/nanobind/src/nb_combined.cpp -std=c++20 -fvisibility=hidden -DNDEBUG -DNB_COMPACT_ASSERTIONS $PYTHON_INCLUDE -fPIC -Iext/nanobind/include -Iext/nanobind/ext/robin_map/include -O3 -fno-strict-aliasing -ffunction-sections -fdata-sections -c -o nobuild/libnanobind.o
 
-g++ -shared -Wl,-s -Wl,--gc-sections nobuild/nobnb.o nobuild/libnanobind.o -o nobuild/nob.cpython-311-x86_64-linux-gnu.so
+g++ -shared -Wl,-s -Wl,--gc-sections nobuild/nobnb.o nobuild/libnanobind.o -o nobuild/nob.cpython-312-x86_64-linux-gnu.so
