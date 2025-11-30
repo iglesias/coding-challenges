cef43c113b33f4deb2e98d2178ffe2cec308d343
Fernando J. Iglesias Garcia <fernando.iglesiasg@gmail.com>
2025-11-30 21:41:13 +0100

    Use the exposed std::map from the python example and fix missing include

--- nob.py
+++ nob.py
@@ -2,3 +2,10 @@ import nobuild.nob as nob
 
 nob.make_and_run_cmd(["ls", "--color", "-l", "-t"])
 nob.make_and_run_cmd(["pwd"])
+
+nob.init_specifics()
+specifics = nob.specifics()
+for file in specifics.keys():
+    print(f'{file}:')
+    for cmd in specifics[file]:
+        print(f'  {cmd.line}')
--- nobnb.cpp
+++ nobnb.cpp
@@ -3,6 +3,7 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/array.h>
 #include <nanobind/stl/map.h>
+#include <nanobind/stl/string.h>
 #include <nanobind/stl/string_view.h>
 #include <nanobind/stl/unique_ptr.h>
 #include <nanobind/stl/vector.h>
