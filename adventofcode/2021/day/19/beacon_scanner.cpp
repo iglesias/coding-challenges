792ce1715302737b42ffbfd31467bfce7996ea3f
Fernando José <fernando.iglesiasg@gmail.com>
2026-02-28 15:19:37 +0100

    WiP: find coordinates of every scanner (other than 0) wrt t0 scanner 0; refactor

--- adventofcode/2021/day/19/beacon_scanner.cpp
+++ adventofcode/2021/day/19/beacon_scanner.cpp
@@ -39,9 +39,17 @@ struct symbol {
     }
 };
 
+namespace math {
+
+constexpr int dim = 3;
+template<typename T> using vector = std::array<T, dim>;
+template<typename T> using matrix = std::array<vector<T>, dim>;
+
+};
+
 struct transformation {
-    std::array<std::array<int, 3>, 3> rotation;
-    std::array<int, 3> translation;
+    math::matrix<int> rotation;
+    math::vector<int> translation;
 };
 
 std::size_t hash_value(symbol const& s) {
@@ -65,16 +73,16 @@ void print(symbol const& s) {
     }
 }
 
-void print(std::array<symbol, 3> const& a) {
+void print(math::vector<symbol> const& a) {
     for (auto const& s : a) {
         print(s);
         std::cout << ' ';
     }
 }
 
-void print(std::array<std::array<int, 3>, 3> matrix) {
+void print(math::matrix<int> A) {
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++)
-        std::cout << std::setw(2) << matrix.at(i).at(j) << (j == 2 ? "\n" : " ");
+        std::cout << std::setw(2) << A.at(i).at(j) << (j == 2 ? "\n" : " ");
 }
 
 // [ 1       0          0     ]
@@ -104,20 +112,20 @@ template<typename T> void Rz(std::array<T, 3>& v) {
     v[1] = -backup;
 }
 
-// vector [a b c]^T is used as "canonical".
-std::array<symbol, 3> const vector = [](){
-        std::array<symbol, 3> vector;
-        vector.at(0).id = 0, vector.at(0).negative = false;
-        vector.at(1).id = 1, vector.at(1).negative = false;
-        vector.at(2).id = 2, vector.at(2).negative = false;
-        return vector;
+// symbolic vector [a b c]^T
+std::array<symbol, 3> const v = [](){
+        std::array<symbol, 3> v;
+        v.at(0).id = 0, v.at(0).negative = false;
+        v.at(1).id = 1, v.at(1).negative = false;
+        v.at(2).id = 2, v.at(2).negative = false;
+        return v;
     }();
 
 auto generate_rotations() {
     std::unordered_set<std::array<symbol, 3>, boost::hash<std::array<symbol, 3>>> Qed;
-    Qed.insert(vector);
+    Qed.insert(v);
     std::queue<std::array<symbol, 3>> Q;
-    Q.push(vector);
+    Q.push(v);
     auto g = [&Q, &Qed](auto const& v, std::function<void(std::array<symbol, 3>&)> R) {
         auto w = v;
         R(w);
@@ -143,13 +151,13 @@ auto rotation_vector_mapping(std::array<symbol, 3> const& item) {
         throw std::runtime_error("Symbol not found.");
     };
 
-    std::array<std::array<int, 3>, 3> matrix;
-    matrix.fill(std::array<int, 3>{0, 0, 0});
+    math::matrix<int> rotation;
+    rotation.fill(std::array<int, 3>{0, 0, 0});
     for (int offset = 0; offset < 3; offset++) {
         int const pos = find_symbol_position(item, 'a'+offset);
-        matrix.at(offset).at(pos) = item.at(pos).negative ? -1 : 1;
+        rotation.at(offset).at(pos) = item.at(pos).negative ? -1 : 1;
     }
-    return matrix;
+    return rotation;
 }
 
 auto read_scan_report(std::istream& input_stream) {
@@ -180,13 +188,22 @@ auto read_input() {
 
 int const mininum_overlapping_beacons = 12;
 
+auto operator*(math::matrix<int> const& A, math::vector<int> const& v) {
+    auto matmul = [A, v](int r){
+        int c = 0;
+        for (int k = 0; k < 3; k++) c += A.at(r).at(k) * v.at(k);
+        return c;
+    };
+    return std::make_tuple(matmul(0), matmul(1), matmul(2));
+}
+
 int main() {
     auto const rotations = generate_rotations();
 
     auto const scans = read_input();
 
     std::unordered_map<int, std::unordered_map<int, transformation>> A;
-    std::unordered_map<int, std::vector<int>> B;
+    std::unordered_map<int, std::vector<int>> neighbors;
 
     for (size_t scan_idx_i = 0; scan_idx_i < scans.size(); scan_idx_i++) {
     for (size_t scan_idx_j = scan_idx_i + 1; scan_idx_j < scans.size(); scan_idx_j++) {
@@ -197,16 +214,9 @@ int main() {
     bool overlap_found = false;
     for (size_t i = 0; i < scan0.size() && !overlap_found; i++) for (size_t j = 0; j < scan1.size() && !overlap_found; j++) {
         for (auto const& item : rotations) {
-            auto const matrix = rotation_vector_mapping(item);
-            // Apply matrix to scan1[j]
-            auto matmul = [&](int r, size_t idx1){
-                int c = 0;
-                for (int k = 0; k < 3; k++) c += matrix.at(r).at(k) * scan1.at(idx1).at(k);
-                return c;
-            };
-            auto const x1 = matmul(0, j);
-            auto const y1 = matmul(1, j);
-            auto const z1 = matmul(2, j);
+            auto const rotation = rotation_vector_mapping(item);
+            // Apply rotation to scan1[j]
+            auto const [x1, y1, z1] = rotation * scan1.at(j);
             // Compute shift from scan1[j] and scan0[i]
             auto const dx = x1 - scan0.at(i).at(0);
             auto const dy = y1 - scan0.at(i).at(1);
@@ -225,9 +235,7 @@ int main() {
             matches.emplace_back(scan0.at(i), scan1.at(j));
             for (size_t jj = 0; jj < scan1.size(); jj++) {
                 if (jj == j) continue;
-                auto const xjj = matmul(0, jj);
-                auto const yjj = matmul(1, jj);
-                auto const zjj = matmul(2, jj);
+                auto const [xjj, yjj, zjj] = rotation * scan1.at(jj);
                 auto const p = std::make_tuple(xjj-dx, yjj-dy, zjj-dz);
                 if (scan0set.contains(p)) {
                     matches_count += 1;
@@ -238,18 +246,18 @@ int main() {
             }
             if (matches_count == mininum_overlapping_beacons) {
                 overlap_found = true;
-                A[scan_idx_i][scan_idx_j] = {matrix, {dx, dy, dz}};
-                B[scan_idx_i].push_back(scan_idx_j);
-                B[scan_idx_j].push_back(scan_idx_i);
+                A[scan_idx_i][scan_idx_j] = {rotation, {-dx, -dy, -dz}};
+                neighbors[scan_idx_i].push_back(scan_idx_j);
+                neighbors[scan_idx_j].push_back(scan_idx_i);
                 std::println("{} -> {}:", scan_idx_i, scan_idx_j);
-                std::println("{}", A[scan_idx_i][scan_idx_j].rotation);
-                std::println("{}\n", A[scan_idx_i][scan_idx_j].translation);
                 if (scan_idx_i == 0 && scan_idx_j == 1) {
+                    /*
                     std::println("Beacons relative to scanner 0:");
                     for (auto const& pq : matches) std::println("  {}", pq.first);
                     std::println("Beacons relative to scanner 1:");
                     for (auto const& pq : matches) std::println("  {}", pq.second);
-                    std::println("Scanner 1 is at {}.", A[0][1].translation); // appears negated
+                    */
+                    std::println("Scanner 1 is at {}", A[0][1].translation);
                 }
                 break;
             }
@@ -260,6 +268,7 @@ int main() {
     }
 
     // Is there more than one connected component?
+    /*
     {
         std::unordered_set<int> c;
         std::queue<int> d;
@@ -268,12 +277,85 @@ int main() {
         while (!d.empty()) {
             int const n = d.front();
             d.pop();
-            for (int const m : B[n]) if (!c.contains(m)) {
+            for (int const m : neighbors[n]) if (!c.contains(m)) {
                 c.insert(m);
                 d.push(m);
             }
         }
         std::println("scans size: {}, #CC: {}", scans.size(), c.size());
+    }
+    std::println();
+    */
+
+    // Paths to scan0:
+    {
+        for (size_t scan_idx = 1; scan_idx < scans.size(); scan_idx++) {
+            std::queue<std::pair</* node state */ int, /* state of the path to scan0 */ std::vector<int>>> Q;
+            Q.emplace(scan_idx, std::vector<int>{});
+            std::unordered_set<int> Qed;
+            Qed.insert(scan_idx);
+            bool scan0_found = false;
+            while (!Q.empty()) {
+                auto [node, path] = Q.front();
+                Q.pop();
+                for (int const neighbor : neighbors[node]) if (!Qed.contains(neighbor)) {
+                    if (neighbor == 0) {
+                        scan0_found = true;
+                        std::print("{} -> ", scan_idx);
+                        for (int const intermediate_node : path) {
+                            std::print("{} -> ", intermediate_node);
+                        }
+                        std::println("0");
+
+                        // Validating on sample input:
+                        int const scan_idx_inspected = 2;
+                        if (scan_idx == scan_idx_inspected) {
+                            math::vector<int> p{0,0,0};
+                            int from = scan_idx_inspected;
+                            for (size_t path_idx = 0; path_idx <= path.size(); path_idx++) {
+                                int const to = path_idx == path.size() ? 0 : path[path_idx];
+                                std::print("  From {} to {}: ", from, to);
+                                if (A.contains(from) && A.at(from).contains(to)) {
+                                    std::println("direct");
+                                    //std::array<std::array<int, 3>, 3> inverse;
+                                    //for (int r = 0; r < 3; r++) for (int c = 0; c < 3; c++)
+                                    //    inverse[r][c] = A[from][to].rotation[c][r];
+                                    // rotate
+                                    //auto const [x, y, z] = matvecmul(inverse, p);
+                                    //auto const [x, y, z] = matvecmul(A[from][to].rotation, p);
+                                    auto const [x, y, z] = A[from][to].rotation * p;
+                                    p[0] = x, p[1] = y, p[2] = z;
+                                    // translate
+                                    for (int c = 0; c < 3; c++) {
+                                        p[c] += A[from][to].translation[c];
+                                    }
+                                } else {
+                                    assert(A.contains(to) && A[to].contains(from));
+                                    std::println("inverse");
+                                    // rotate
+                                    //auto const [x, y, z] = matvecmul(inverse, p);
+                                    //auto const [x, y, z] = matvecmul(A[to][from].rotation, p);
+                                    auto const [x, y, z] = A[to][from].rotation * p;
+                                    p[0] = x, p[1] = y, p[2] = z;
+                                    // translate
+                                    for (int c = 0; c < 3; c++) {
+                                        p[c] += A[to][from].translation[c];
+                                    }
+                                }
+                                from = to;
+                            }
+                            std::println("Scanner {} is at {}", scan_idx, p);
+                        }
+
+                        break;
+                    } else {
+                        path.push_back(neighbor);
+                        Qed.insert(neighbor);
+                        Q.emplace(neighbor, path);
+                    }
+                }
+            }
+        }
     }
 }
 }
